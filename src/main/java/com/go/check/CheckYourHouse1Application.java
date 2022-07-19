d6a1f59b3d78280fa33b1e58bee9596e4af8e338
ghryu <dodnam@naver.com>
2022-07-19 20:36:16 +0900

    최초 커밋

---
+++ src/main/java/com/go/check/CheckYourHouse1Application.java
@@ -0,0 +1,13 @@
+package com.go.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckYourHouse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckYourHouse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/go/check/CheckYourHouse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckYourHouse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
